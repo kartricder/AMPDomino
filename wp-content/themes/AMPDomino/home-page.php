211781f69bffc05fdf889c0023de9777822ebb03
kartricder <kartricder@gmail.com>
2019-10-29 17:29:54 +0700

    upload local

---
+++ wp-content/themes/AMPDomino/home-page.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<?php
+/*
+ *  Template Name: Home Page Domino
+ * */
+?>
+
+<html ⚡="" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Domino Glass Kitchen</title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <link rel="canonical" href="https://tubepdomino/home-page.php/">
+    
+    <style amp-boilerplate="">body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate="">body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
+
+    <style amp-custom="">
+        html {
+            font-family: sans-serif;
+            line-height: 1.15;
+            -ms-text-size-adjust: 100%;
+            -webkit-text-size-adjust: 100%
+        }
+        body {
+         margin: 0
+        }
+        .relative-class{
+            position: relative;
+        }
+        .header-text {
+            position: absolute;
+            top: 50%;
+            width: 100%;
+            text-align: center;
+            font-size: 100px;
+            font-weight: 700;
+            line-height: 100px;
+            color: 
+            #fff;
+            letter-spacing: 15px;
+        }
+    </style>
+</head>
+<body>
+    <!-- Header Start -->
+    <div class="amp-header-lg">
+        <a href="#"> <amp-img class="logo-amp" src="" width></amp-img> </a>
+    </div>
+    <!--  -->
+    <!-- Background Image -->
+    <div class="relative-class">
+        <amp-img class="commerce-hero-image" src="<?php the_field('image_background')?>" width="1920" height="1080"
+          layout="responsive" alt="Hero Landing 1" noloading="">
+        </amp-img>
+        <div class="header-text">DOMINO</div>
+    </div>
+    <!--  -->
+</body>
+</html>
